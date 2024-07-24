f3f0f9f315de645d82e5270c71ec2d9fdb8c0e55
Am1d1v <dimadiman1adima@gmail.com>
2024-07-24 13:01:14 +0300

    Draw paddle on canvas

--- script.js
+++ script.js
@@ -17,6 +17,16 @@ const ball = {
     dy: -4
 };
 
+// Create paddle
+const paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    width: 80,
+    height: 10,
+    speed: 8,
+    dx: 0
+};
+
 // Draw ball on canvas
 function drawBall(){
     context.beginPath();
@@ -26,6 +36,16 @@ function drawBall(){
     context.closePath();
 };
 drawBall();
+
+// Draw paddle on canvas
+function drawPaddle(){
+    context.beginPath();
+    context.rect(paddle.x, paddle.y, paddle.width, paddle.height);
+    context.fillStyle = '#000';
+    context.fill();
+    context.closePath();
+}
+drawPaddle();
 
 // Show Rules
 rulesButton.addEventListener('click', () => rules.classList.add('show'));
